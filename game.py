369392b27d702a7c5ff5b97ecf3ea98888df177e
x74717zz <zhaojian.zhen@student.manchester.ac.uk>
2020-11-19 04:11:46 +0000

    Reworked menu again so that there is only one menu function which has access to all the other types of menus.

--- game.py
+++ game.py
@@ -1,92 +1,64 @@
 from tkinter import Tk, PhotoImage, Label, Button, Canvas, Frame, CENTER, NW, SW, BOTH
 
-def main_menu():
-	"""Main menu"""
+def menu(menutype="default"):
+	"""Creates a menu, takes arguments such that different menus can be chosen"""
 
 	canvas.delete("fg")
 
-	title_label = Label(window, image=title_image)
-	title_canvaswindow = canvas.create_window(450,100, anchor=CENTER, window=title_label, tags="fg")
-	
-	play_button = Button(window, text="Play", font = ("Arial", 50), command=play_menu)
-	play_canvaswindow = canvas.create_window(450,350, anchor=CENTER, window=play_button, tags="fg")
+	back_button = Button(window, text="Back", font = ("Arial", 50), command=menu)
 
-	howtoplay_button = Button(window, text="How to Play", font = ("Arial", 50), command=howtoplay_menu)
-	howtoplay_canvaswindow = canvas.create_window(450,550, anchor=CENTER, window=howtoplay_button, tags="fg")
+	if menutype == "play":
 
-	about_button = Button(window, text="About", font = ("Arial", 50), command=about_menu)
-	about_canvaswindow = canvas.create_window(450,750, anchor=CENTER, window=about_button, tags="fg")
+		ship = canvas.create_image(450,540,image = ship_image, tags="fg")
 
-	exit_button = Button(window, text="Exit", font = ("Arial", 50), command=exit)
-	exit_canvaswindow = canvas.create_window(450,950, anchor=CENTER, window=exit_button, tags="fg")
+		choose_button = Button(window, text="Choose", font = ("Arial", 50),  command=lambda menutype="difficulty": menu(menutype))
+		choose_canvaswindow = canvas.create_window(450,800, anchor=CENTER, window=choose_button, tags="fg")
 
-def play_menu():
-	"""Menu for when play is chosen, can choose ships"""
+		back_canvaswindow = canvas.create_window(0,1080, anchor=SW, window=back_button, tags="fg")
 
-	canvas.delete("fg")
 
-	title_label = Label(window, image=title_image)
-	title_canvaswindow = canvas.create_window(450,100, anchor=CENTER, window=title_label, tags="fg")
+	elif menutype == "difficulty":
 
-	ship = canvas.create_image(450,540,image = ship_image, tags="fg")
+		easy_button = Button(window, text="Easy", font = ("Arial", 50), command = lambda difficulty = "easy", ship = "ship": game_loop(difficulty, ship))
+		normal_button = Button(window, text="Normal", font = ("Arial", 50), command = lambda difficulty = "normal", ship = "ship": game_loop(difficulty, ship))
+		hard_button = Button(window, text="Hard", font = ("Arial", 50), command = lambda difficulty = "hard", ship = "ship": game_loop(difficulty, ship))
 
-	play_button = Button(window, text="Choose", font = ("Arial", 50),  command=lambda: difficulty_menu(ship))
-	play_canvaswindow = canvas.create_window(450,800, anchor=CENTER, window=play_button, tags="fg")
+		easy_canvaswindow = canvas.create_window(450, 350, anchor=CENTER, window=easy_button, tags="fg")
+		normal_canvaswindow = canvas.create_window(450, 550, anchor=CENTER, window=normal_button, tags="fg")
+		hard_canvaswindow = canvas.create_window(450, 750, anchor=CENTER, window=hard_button, tags="fg")#
 
-	back_button = Button(window, text="Back", font = ("Arial", 50), command=main_menu)
-	back_canvaswindow = canvas.create_window(0,1080, anchor=SW, window=back_button, tags="fg")
 
-def howtoplay_menu():
-	"""How to play the game including controls and objectives"""
+		back_button.config(command = lambda menutype="play": menu(menutype))
+		back_canvaswindow = canvas.create_window(0,1080, anchor=SW, window=back_button, tags="fg")
 
-	canvas.delete("fg")
 
-	title_label = Label(window, image=title_image)
-	title_canvaswindow = canvas.create_window(450,100, anchor=CENTER, window=title_label, tags="fg")
+	elif menutype == "settings":
 
-	howtoplay_label = Label(window, image=howtoplay_image)
-	howtoplay_canvaswindow = canvas.create_window(450,800, anchor=CENTER, window=howtoplay_label, tags="fg")
+		settings_label = Label(window, text="Settings")
+		settings_canvaswindow = canvas.create_window(450,800, anchor=CENTER, window=settings_label, tags="fg")
 
-	back_button = Button(window, text="Back", font = ("Arial", 50), command=main_menu)
-	back_canvaswindow = canvas.create_window(0,1080, anchor=SW, window=back_button, tags="fg")
-			
-def about_menu():
-	"""Menu with information about the game and credits"""
+		back_canvaswindow = canvas.create_window(0,1080, anchor=SW, window=back_button, tags="fg")
 
-	canvas.delete("fg")
+	elif menutype == "about":
 
-	title_label = Label(window, image=title_image)
-	title_canvaswindow = canvas.create_window(450,100, anchor=CENTER, window=title_label, tags="fg")
+		about_label = Label(window, image=about_image)
+		about_canvaswindow = canvas.create_window(450,800, anchor=CENTER, window=about_label, tags="fg")
 
-	about_label = Label(window, image=about_image)
-	about_canvaswindow = canvas.create_window(450,800, anchor=CENTER, window=about_label, tags="fg")
+		back_canvaswindow = canvas.create_window(0,1080, anchor=SW, window=back_button, tags="fg")		
 
-	back_button = Button(window, text="Back", font = ("Arial", 50), command=main_menu)
-	back_canvaswindow = canvas.create_window(0,1080, anchor=SW, window=back_button, tags="fg")
+	else:
 
-def difficulty_menu(ship):
-	"""Menu to choose difficulty"""
+		title_label = Label(window, image=title_image)
+		play_button = Button(window, text="Play", font = ("Arial", 50), command= lambda menutype="play": menu(menutype))
+		settings_button = Button(window, text="Settings", font = ("Arial", 50), command= lambda menutype="settings": menu(menutype))
+		about_button = Button(window, text="About", font = ("Arial", 50), command= lambda menutype="about": menu(menutype))
+		exit_button = Button(window, text="Exit", font = ("Arial", 50), command= lambda: window.destroy())
 
-	canvas.delete("fg")
-
-	title_label = Label(window, image=title_image)
-	title_canvaswindow = canvas.create_window(450,100, anchor=CENTER, window=title_label, tags="fg")
-
-	easy_button = Button(window, text="Easy", font = ("Arial", 50), command = lambda: game_loop("easy", ship))
-	normal_button = Button(window, text="Normal", font = ("Arial", 50), command = lambda: game_loop("normal", ship))
-	hard_button = Button(window, text="Hard", font = ("Arial", 50), command = lambda: game_loop("hard", ship))
-
-	easy_canvaswindow = canvas.create_window(450, 350, anchor=CENTER, window=easy_button, tags="fg")
-	normal_canvaswindow = canvas.create_window(450, 550, anchor=CENTER, window=normal_button, tags="fg")
-	hard_canvaswindow = canvas.create_window(450, 750, anchor=CENTER, window=hard_button, tags="fg")
-
-	back_button = Button(window, text="Back", font = ("Arial", 50), command=play_menu)
-	back_canvaswindow = canvas.create_window(0,1080, anchor=SW, window=back_button, tags="fg")
-
-def exit():
-	"""Closes the game"""
-
-	window.destroy()
+		title_canvaswindow = canvas.create_window(450,100, anchor=CENTER, window=title_label, tags="fg")
+		play_canvaswindow = canvas.create_window(450,350, anchor=CENTER, window=play_button, tags="fg")
+		settings_canvaswindow = canvas.create_window(450,550, anchor=CENTER, window=settings_button, tags="fg")
+		about_canvaswindow = canvas.create_window(450,750, anchor=CENTER, window=about_button, tags="fg")
+		exit_canvaswindow = canvas.create_window(450,950, anchor=CENTER, window=exit_button, tags="fg")
 
 def game_loop(difficulty, ship):
 	"""The main game"""
@@ -104,11 +76,10 @@ canvas.pack()
 
 title_image = PhotoImage(file="Assets/placeholder.png")
 
-howtoplay_image = PhotoImage(file="Assets/placeholder.png")
 about_image = PhotoImage(file="Assets/placeholder.png")
 
 ship_image = PhotoImage(file="Assets/aship1.gif")
 
-main_menu()
+menu()
 
 window.mainloop()
